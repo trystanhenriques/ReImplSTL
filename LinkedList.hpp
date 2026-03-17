e53374dc1faeb694b22290b5d1b4ffce9a90bfa0
Trystan Henriques <trystanhenriquesgit@gmail.com>
2026-03-16 20:02:59 -0400

    Add push_front rvalue overload
    Introduce push_front(value_type&&) to prepend a moved value. The new method allocates a Node with std::move, sets m_head/m_tail when the list is empty, relinks the previous head to the new node, and increments m_size. Note: the current implementation increments m_size twice for an empty list (once inside the empty branch and again at the end).

--- LinkedList.hpp
+++ LinkedList.hpp
@@ -386,6 +386,32 @@ public:
 		++m_size;	
 	}
 
+	// Prepends a move of value to the beginning of the container.
+	void push_front(value_type&& rval) {
+
+		// Create new Node
+		Node* newNode{ new Node {std::move(rval), nullptr, nullptr} };
+
+		// if empty, set head and tail to new Node
+		if (empty()) {
+			m_head = newNode;
+			m_tail = newNode;
+			++m_size;
+		}
+
+		// Make the previous head point to the new Node
+		m_head->prev = newNode;
+
+		// Set the newNodes next to point at the old head
+		newNode->next = m_head;
+
+		// Set the new head
+		m_head = newNode;
+
+		// Increase size
+		++m_size;
+	}
+
 	
 	// Removes the last element of the container.
 	void pop_back() {
